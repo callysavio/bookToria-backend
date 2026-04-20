ee639ed4affd8a1a7e7be9783111ba069f032b65
callysavio <callistusanwara@gmail.com>
2026-04-20 15:48:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+//create an instance of express server object:
+const app = express();
+const PORT = process.env.PORT;
+app.get("/", function (req, res) {
+  res.send("Welcome to Book-toria backend!");
+});
+app.listen(PORT, function () {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
