5db6e32adacf9aa3299f2a07eeb1d593feb6c76d
callysavio <callistusanwara@gmail.com>
2026-04-21 16:45:57 +0100

    user model created

---
+++ src/models/user.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+// Define the User schema
+const userSchema = new mongoose.Schema(
+  {
+    profilePicture: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/128/2202/2202112.png",
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 6,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+// Create the User model
+const User = mongoose.model("User", userSchema);
+
+export default User;
