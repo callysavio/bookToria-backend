41bfaa4de6d5fd069c4f8cb62cc0deab0115e4fd
Abdulghalib Adebola <deybola_xm@Deybolas-MacBook-Pro.local>
2026-04-21 16:55:12 +0100

    first commit

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
