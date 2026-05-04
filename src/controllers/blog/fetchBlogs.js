991bbbd90a29cc1731744bc722d55865fca077f6
A.Gabriel <adenrelegabriel@gmail.com>
2026-05-04 16:32:48 +0100

    Fix blog fetch endpoint

---
+++ src/controllers/blog/fetchBlogs.js
@@ -0,0 +1,24 @@
+import httpStatus from "http-status";
+import Blog from "../../models/blog.js";
+
+export const fetchBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ isPublished: true })
+      .sort({ createdAt: -1 })
+      .limit(10);
+
+    return res.status(httpStatus.OK).json({
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Blogs fetched successfully",
+      data: blogs,
+    });
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      success: false,
+      message: "An error occurred while fetching blogs",
+      error: error.message,
+    });
+  }
+};
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
 import { create } from "../controllers/blog/create.js";
+import { fetchBlogs } from "../controllers/blog/fetchBlogs.js";
 
-// Define aliases for creating a blog post
+// Create aliases for creating a blog post
 router.post("/", create);
 router.post("/create", create);
+
+// Fetch blogs
+router.get("/", fetchBlogs);
+router.get("/fetch", fetchBlogs);
 
 export default router;
