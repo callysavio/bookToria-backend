1d607b148eda8a01d2d60e6646a2e3b6ae11044a
jaynuel10 <johnsonaldoceo100@gmail.com>
2026-05-05 17:07:45 +0100

    commit

---
+++ src/controllers/blog/updateBlog.js
@@ -0,0 +1,40 @@
+import Blog from "../../models/blog.js";
+import httpStatus from "http-status";
+
+// Controller for updating a blog post
+const updateBlog = async (req, res) => {
+  try {
+    // 1. Extract blog post ID from the request params
+    const { id } = req.params;
+    // 2. Get the data to be updated from the request body
+    const { title, author, content } = req.body;
+    // 3. Find the blog post by ID and update the specified fields
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id,
+      { title, author, content },
+      { new: true }, // Return the updated document
+    );
+    if (!updatedBlog) {
+      return res.status(404).json({
+        statusCode: 404,
+        success: false,
+        message: "Blog post not found",
+      });
+    }
+    return res.status(200).json({
+      statusCode: 200,
+      success: true,
+      message: "Blog post updated successfully",
+      data: updatedBlog,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      statusCode: 500,
+      success: false,
+      message: "Error updating blog post",
+      error: error.message,
+    });
+  }
+};
+
+export { updateBlog };
