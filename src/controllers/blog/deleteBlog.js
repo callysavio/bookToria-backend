998679c71ef4b8c32e8a0a4d784c11fb0d6a11f2
mdmuche <abulamartins@gmail.com>
2026-05-07 15:47:16 +0100

    feat: implement delete blog

---
+++ src/controllers/blog/deleteBlog.js
@@ -0,0 +1,34 @@
+import Blog from "../../models/blog.js";
+import httpStatus from "http-status";
+//controller for deleting a blog
+export const deleteBlog = async (req, res) => {
+  try {
+    //1. get blog id from the request parameter
+    const { id } = req.params;
+    //2. check if blog exists
+    let blog = await Blog.findById(id);
+    if (!blog) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        status: httpStatus.NOT_FOUND,
+        success: false,
+        message: "Blog not found",
+      });
+    }
+    //3. delete the blog
+    await Blog.findByIdAndDelete(id);
+    //4. return success response
+    return res.status(httpStatus.OK).json({
+      statusCode: httpStatus.OK,
+      success: true,
+      message: "Blog deleted successfully",
+    });
+    //5. handle errors
+  } catch (error) {
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      success: false,
+      message: "Error deleting blog",
+      error: error.message,
+    });
+  }
+};
