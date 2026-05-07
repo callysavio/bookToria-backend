35b6cdde95e2017ffc7e77ddf5fe5fab1e665e2a
manny <onwukasomtochukwu2024@gmail.com>
2026-05-07 15:45:51 +0100

    feat: implement delete functionality for blog posts and add route endpoint

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -3,9 +3,11 @@ import { createBlog } from "../controllers/blog/create.js";
 import fetchBlogs from "../controllers/blog/fetchBlogs.js";
 import fetchBlogById from "../controllers/blog/fetchBlogById.js";
 import { updateBlog } from "../controllers/blog/updateBlog.js";
+import { deleteBlog } from "../controllers/blog/deleteBlog.js";
 const router = express.Router();
 router.post("/create", createBlog);
 router.get("/fetch", fetchBlogs);
 router.get("/details/:id", fetchBlogById);
 router.put("/update/:id", updateBlog);
+router.delete("/delete/:id", deleteBlog)
 export default router;
