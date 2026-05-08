d30b7ec05a343fc63c26ee9e30020c33d333413a
callysavio <callistusanwara@gmail.com>
2026-05-08 16:28:30 +0100

    validation with middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,13 @@
 import express from "express";
 const router = express.Router();
+
+import { registerValidationSchema } from "../validators/auth/register.js";
+import { validate } from "../middlewares/validate.js";
 import { register } from "../controllers/users/register.js";
 import { updateUser } from "../controllers/users/update.js";
 import { deleteUser } from "../controllers/users/delete.js";
 // Define the route for user registration
-router.post("/register", register);
+router.post("/register", validate(registerValidationSchema), register);
 router.put("/update/:id", updateUser);
 router.delete("/delete/:id", deleteUser);
 export default router;
