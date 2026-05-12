aa1c40bf4b094c112963f27e4d13520fe3a9226b
mdmuche <abulamartins@gmail.com>
2026-05-12 15:48:08 +0100

    add validate for login, update and delete user

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,9 +7,12 @@ import { register } from "../controllers/auth/register.js";
 import { login } from "../controllers/auth/login.js";
 import { updateUser } from "../controllers/users/update.js";
 import { deleteUser } from "../controllers/users/delete.js";
+import { loginValidationSchema } from "../validators/auth/login.js";
+import { updateValidationSchema } from "../validators/auth/update.js";
+import { deleteValidationSchema } from "../validators/auth/delete.js";
 // Define the route for user registration
 router.post("/register", validate(registerValidationSchema), register);
-router.post("/login", login);
-router.put("/update/:id", updateUser);
-router.delete("/delete/:id", deleteUser);
+router.post("/login", validate(loginValidationSchema), login);
+router.put("/update/:id", validate(updateValidationSchema), updateUser);
+router.delete("/delete/:id", validate(deleteValidationSchema), deleteUser);
 export default router;
